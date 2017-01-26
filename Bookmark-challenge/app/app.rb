5e935a526eeb2948d9a0dbb440e835aae443fa9d
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-26 17:47:34 +0000

    Finish 18

--- Bookmark-challenge/app/app.rb
+++ Bookmark-challenge/app/app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     redirect '/links'
@@ -38,6 +40,22 @@ class BookmarkManager < Sinatra::Base
     @links = Link.all
     @search = params["Search tags"]
     erb :'tags/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   # start the server if ruby file executed directly
--- Bookmark-challenge/spec/features/web_helpers.rb
+++ Bookmark-challenge/spec/features/web_helpers.rb
@@ -12,3 +12,11 @@ def create_and_tag
   fill_in :tags, :with => "Social Media"
   click_button 'Create Link'
 end
+
+def sign_up
+  visit '/users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :email,    with: 'alice@example.com'
+  fill_in :password, with: 'oranges!'
+  click_button 'Sign up'
+end
