7f839af91fc35317a63afc0f2dc7432db41a600e
John Ashton <John_ashton@hotmail.com>
2017-01-26 12:20:48 +0000

    exercise 16 tag search functionality

--- Bookmark-challenge/spec/features/filter_by_tag_spec.rb
+++ Bookmark-challenge/spec/features/filter_by_tag_spec.rb
@@ -4,8 +4,8 @@ feature 'filtering by tags' do
   scenario 'User can filter by tags' do
     create_and_tag
     click_button('Filter tags')
-    fill_in 'Search tag', :with => 'Social Media'
-    click_button('Search tag')
+    fill_in 'Search tags', :with => 'Social Media'
+    click_button('Search Link')
     expect(page).to have_content('Social Media')
   end
 end
